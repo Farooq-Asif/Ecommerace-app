e41f9093565029bf9ffeb8b6e3a0e818fe003f02
Farooq-Asif <chsaab983@gmail.com>
2024-08-16 23:30:41 +0500

    add skelton

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Store from './Components/redux/Store';
 import { createContext, useState } from 'react';
+import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+
 export const SomeContext = createContext()
 function App() {
   const [length, setlength] = useState()
   // console.log(length);
   return (
     <div className="App">
+       <SkeletonTheme baseColor="#202020" highlightColor="#c0cbcd">
       <SomeContext.Provider value={{ setlength, length }}>
         <Provider store={Store}>
           <BrowserRouter>
@@ -18,6 +21,7 @@ function App() {
           </BrowserRouter>
         </Provider>
       </SomeContext.Provider>
+      </SkeletonTheme>
     </div>
   );
 }
